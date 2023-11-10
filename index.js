cb030aba74c009ed856e3fc7081fda6394926ce1
Justabingereader <Harounimran04@gmail.com>
2023-11-10 23:16:08 +0100

    changes made

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const keys = require('./config/keys');
+require('dotenv').config();
 
 require('./models/user');
 require('./services/passport');
 
 
  
-mongoose.connect(keys.mongoURI);
+mongoose.connect(process.env.MONGO_URI);
 
 const app = express();
 
 app.use(
     cookieSession({
         maxAge: 30 *24*60*60*1000,
-        keys: [keys.cookieKey]
+        keys: [process.env.COOKIE_KEY]
     })
 );
 
--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
-const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+require('dotenv').config();
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -18,8 +18,8 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'https://emaily-v89q.onrender.com/auth/google/callback',
       proxy: true,
     },
